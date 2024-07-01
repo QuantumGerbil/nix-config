1217819b95d83b40c83b8b436e23419a06b24517
QuantumGerbil <bpmorgan@pm.me>
2024-07-01 18:33:16 -0500

    fix sudo

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,7 +56,7 @@
       # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
       # Opinionated: disable global registry
-      flake-registry = "";
+      #flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
@@ -93,6 +93,28 @@
   services.xserver.xkb.layout = "us";
   services.printing.enable = true;
   services.displayManager.sddm.enable = true;
+  programs.hyprland.enable = true;
+  
+  security.sudo = {
+  enable = true;
+  extraRules = [{
+    commands = [
+      {
+        command = "${pkgs.systemd}/bin/systemctl suspend";
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "${pkgs.systemd}/bin/reboot";
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "${pkgs.systemd}/bin/poweroff";
+        options = [ "NOPASSWD" ];
+      }
+    ];
+    groups = [ "wheel" ];
+    }];
+  };
 
   services.pipewire = {
     enable = true;
@@ -134,18 +156,12 @@
   #system.copySystemConfiguration = true;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
-  users.users = {
-    bmorgan = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
+  users.users.bmorgan = {
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
-        # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHV3qLddiS8FSfJSm7q+nSyPTBDax6TjlWS02AL6pmtL bpmorgan@pm.me"
       ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel audio networkmanager input"];
-    };
+      extraGroups = ["wheel" "audio" "networkmanager" "input"];
   };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
